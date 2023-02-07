deb379f4e3566440240e827073e83492e7c624e2
stiven muñoz usuga <stivenusuga019@gmail.com>
2023-02-07 14:48:46 -0500

    Coleque string y cambie fondo

--- app/src/main/java/com/example/turistiando/Hoteles.java
+++ app/src/main/java/com/example/turistiando/Hoteles.java
@@ -1,11 +1,19 @@
 package com.example.turistiando;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Hoteles extends AppCompatActivity {
 
@@ -17,6 +25,11 @@ public class Hoteles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoteles);
+        ActionBar barra=getSupportActionBar();
+
+        //Cambiando el color del action bar
+        ColorDrawable colorBarra= new ColorDrawable(Color.parseColor("#DC2D50"));
+        barra.setBackgroundDrawable(colorBarra);
 
         listaLogica=findViewById(R.id.recycler);
         listaLogica.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
@@ -28,11 +41,57 @@ public class Hoteles extends AppCompatActivity {
     }
     public void crearListaHoteles(){
 
-        lista.add(new Hotel(R.drawable.hotel1,"Hotel 1","Muchas diversion con tu familia y mascotas","$150.000"));
-        lista.add(new Hotel(R.drawable.hotel2,"Hotel 2","Muchas diversion con tu Pareja","$100.000"));
-        lista.add(new Hotel(R.drawable.hotel3,"Hotel 3","Hotel para pasar con tu familia , mascotas y amigos con turismo y conocimientos nuevos","$120.000"));
-        lista.add(new Hotel(R.drawable.hotel4,"Hotel 4","Un Hotel con vista al mar para que pases bueno y disfrutes de nuevas experiencias y conoscas nuevas amistades o encontrar el Amor","$200.000"));
+        lista.add(new Hotel(R.drawable.hotel1,Integer.toString(R.string.NombreHotel1),Integer.toString(R.string.descripcionhotel1),Integer.toString(R.string.PrecioHotel1)));
+        lista.add(new Hotel(R.drawable.hotel2,Integer.toString(R.string.NombreHotel2),Integer.toString(R.string.descripcionhotel2),Integer.toString(R.string.PrecioHotel2)));
+        lista.add(new Hotel(R.drawable.hotel3,Integer.toString(R.string.NombreHotel3),Integer.toString(R.string.descripcionhotel3),Integer.toString(R.string.PrecioHotel3)));
+        lista.add(new Hotel(R.drawable.hotel4,Integer.toString(R.string.NombreHotel4),Integer.toString(R.string.descripcionhotel4),Integer.toString(R.string.PrecioHotel4)));
 
+    }
+    public void cambiarIdioma(String idioma){
+
+        //Configurando el lenguaje del celular
+        Locale lenguaje= new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configuración global en el celular
+
+        Configuration configurationTelefono=getResources().getConfiguration();
+        configurationTelefono.locale=lenguaje;
+
+        //ejecutamos la configuración
+        getBaseContext().getResources().updateConfiguration(configurationTelefono,getBaseContext().getResources().getDisplayMetrics());
+
+    }
+    //Cargar el menu xml creado previamente
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    //DAR funcionamiento al menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //observamos que item del menu le damos click
+
+        int itemSeleccionado=item.getItemId();
+        switch (itemSeleccionado){
+
+            case(R.id.opcion1):
+                Intent IntentA = new Intent(getApplicationContext(),Acercade.class);
+                startActivity(IntentA);
+                break;
+            case(R.id.opcion2):
+                this.cambiarIdioma("en");
+                Intent intent3 = new Intent(Hoteles.this,Hoteles.class);
+                startActivity(intent3);
+                break;
+            case(R.id.opcion3):
+                this.cambiarIdioma("es");
+                Intent intent4= new Intent(Hoteles.this,Hoteles.class);
+                startActivity(intent4);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
--- app/src/main/java/com/example/turistiando/Restaurantes.java
+++ app/src/main/java/com/example/turistiando/Restaurantes.java
@@ -1,11 +1,19 @@
 package com.example.turistiando;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Restaurantes extends AppCompatActivity {
 
@@ -17,6 +25,12 @@ public class Restaurantes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurantes);
 
+        ActionBar barra=getSupportActionBar();
+
+        //Cambiando el color del action bar
+        ColorDrawable colorBarra= new ColorDrawable(Color.parseColor("#DC2D50"));
+        barra.setBackgroundDrawable(colorBarra);
+
         listaLogica=findViewById(R.id.recycler2);
         listaLogica.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
 
@@ -26,11 +40,57 @@ public class Restaurantes extends AppCompatActivity {
     }
     public void crearListaRestaurante(){
 
-        lista2.add(new Hotel(R.drawable.restaurante32,Integer.toString(R.string.Nombre),"Muchas diversion con tu familia y mascotas","$150.000"));
-        lista2.add(new Hotel(R.drawable.restaurante2,"Restaurante 2","Muchas diversion con tu Pareja","$100.000"));
-        lista2.add(new Hotel(R.drawable.restaurante4,"Restaurante 3","Hotel para pasar con tu familia , mascotas y amigos con turismo y conocimientos nuevos","$120.000"));
-        lista2.add(new Hotel(R.drawable.restaurante45,"Restaurante 4","Un Hotel con vista al mar para que pases bueno y disfrutes de nuevas experiencias y conoscas nuevas amistades o encontrar el Amor","$200.000"));
+        lista2.add(new Hotel(R.drawable.restaurante32,Integer.toString(R.string.NombreRes1),Integer.toString(R.string.desRestaurante1),Integer.toString(R.string.PrecioRes1)));
+        lista2.add(new Hotel(R.drawable.restaurante2,Integer.toString(R.string.NombreRes2),Integer.toString(R.string.desRestaurante2),Integer.toString(R.string.PrecioRes2)));
+        lista2.add(new Hotel(R.drawable.restaurante4,Integer.toString(R.string.NombreRes3),Integer.toString(R.string.desRestaurante3),Integer.toString(R.string.PrecioRes3)));
+        lista2.add(new Hotel(R.drawable.restaurante45,Integer.toString(R.string.NombreRes4),Integer.toString(R.string.desRestaurante4),Integer.toString(R.string.PrecioRes4)));
 
+    }
+    public void cambiarIdioma(String idioma){
+
+        //Configurando el lenguaje del celular
+        Locale lenguaje= new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configuración global en el celular
+
+        Configuration configurationTelefono=getResources().getConfiguration();
+        configurationTelefono.locale=lenguaje;
+
+        //ejecutamos la configuración
+        getBaseContext().getResources().updateConfiguration(configurationTelefono,getBaseContext().getResources().getDisplayMetrics());
+
+    }
+    //Cargar el menu xml creado previamente
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    //DAR funcionamiento al menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //observamos que item del menu le damos click
+
+        int itemSeleccionado=item.getItemId();
+        switch (itemSeleccionado){
+
+            case(R.id.opcion1):
+                Intent IntentA = new Intent(getApplicationContext(),Acercade.class);
+                startActivity(IntentA);
+                break;
+            case(R.id.opcion2):
+                this.cambiarIdioma("en");
+                Intent intent3 = new Intent(Restaurantes.this,Restaurantes.class);
+                startActivity(intent3);
+                break;
+            case(R.id.opcion3):
+                this.cambiarIdioma("es");
+                Intent intent4= new Intent(Restaurantes.this,Restaurantes.class);
+                startActivity(intent4);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
